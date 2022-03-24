f3aba89e80aba8d39e0abe922ba4b5d2721c031b
Mauricio <mauriciomage@gmail.com>
2022-03-24 18:12:18 +0100

    components and services needed created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { ListComponent } from './components/list/list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ListComponent,
+    NavbarComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule
